62c2c906a5c5c936825baeea38717723716d96da
“Justkelvn” <olowodepraise@gmail.com>
2023-06-23 13:00:10 -0700

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,6 @@
-# Creates a file
-file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+file {'/tmp/school':
+    mode       => '0744',
+    owner      => www-data,
+    group      => www-data,
+    content    => 'I love Puppet'
 }
